c8a264c425b250cbfb8eb95b017f3de5fa365584
joseph.cardillo <joseph.s.cardillo@gmail.com>
2018-04-13 17:13:57 -0400

    Created comment folder in views and posts/comments form. Closes #16, closes #17

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,14 @@
 class PostsController < ApplicationController
   include PostsHelper
+
   def index
     @posts = Post.all
   end
 
   def show
     @post = Post.find(params[:id])
+    @comment = Comment.new
+    @comment.post_id = @post.id
   end
 
   def new
