228d72ecfdaf4d16e0ffd15a5f6eea261d1fa857
joseph.cardillo <joseph.s.cardillo@gmail.com>
2018-04-13 16:24:29 -0400

    added edit link to posts show.html.erb file, closes #10

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,11 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
+
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
