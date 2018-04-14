4ac7cf019ccb88f0f5a2f3fe4dcc1ed0d35c4caa
joseph.cardillo <joseph.s.cardillo@gmail.com>
2018-04-14 07:43:22 -0400

    Create destroy action for tags. Closes #33

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,10 +2,17 @@ class TagsController < ApplicationController
 
   def index
     @tags = Tag.all
-    
+
   end
 
   def show
     @tag = Tag.find(params[:id])
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+    flash.notice = "Your tag titled '#{@tag.name}' has been deleted!"
+    redirect_to tags_path
   end
 end
