ed45c505b17fd8e0536cd8f2602bead13a8b7b32
joseph.cardillo <joseph.s.cardillo@gmail.com>
2018-04-17 13:24:04 -0400

    posts now show on user profile page, closes #43

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,8 +21,8 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    # @user = User.find_by(username: params[:username])
-    # @posts = @user.posts.reverse
+    @user = User.find(params[:id])
+    @posts = @user.posts.reverse
   end
 
   # GET /users/new
