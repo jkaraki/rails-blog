4e3f5f37649263f202ba2187f4864aa30b031bf8
joseph.cardillo <joseph.s.cardillo@gmail.com>
2018-04-17 14:43:51 -0400

    Can now save new comments to DB. Closes #49.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,15 +3,20 @@ class CommentsController < ApplicationController
   before_action :require_login
 
   def create
+    puts '***************************'
+    puts params.inspect
+    puts '***************************'
     @comment = Comment.new(comment_params)
-    @post = Post.find(params[:id])
+    # @post = Post.find(params[:id])
     @comment.post_id = params[:post_id]
     @comment.save
     flash.notice = "Your comment has been posted!"
     redirect_to post_path(@comment.post)
   end
 
+  private
+
   def comment_params
-    params.require(:comment).permit(:username, :body, :created_at)
+    params.require(:comment).permit(:username, :body, :created_at, :post_id)
   end
 end
